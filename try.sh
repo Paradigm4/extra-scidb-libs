0540e81707979baf7ceb6d0c3b3a99a58d250664
Rares Vernica <rvernica@gmail.com>
2018-05-13 11:51:50 -0700

    Test for SciDB version in shim

--- try.sh
+++ try.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
+set -o errexit
+
 iquery --afl --query "load_library('accelerated_io_tools')"
 iquery --afl --query "load_library('equi_join')"
 iquery --afl --query "load_library('grouped_aggregate')"
 iquery --afl --query "load_library('stream')"
 iquery --afl --query "load_library('superfunpack')"
+
+echo "SciDB version in shim..."
+shim -version | grep "SciDB Version: $SCIDB_VER"
