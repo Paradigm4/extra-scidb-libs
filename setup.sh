f76975cd94d48795a68ed24ebd51e7f20971f0dd
Rares Vernica <rvernica@gmail.com>
2018-08-05 17:44:09 -0700

    Manually install devtoolset-3 packages

--- setup.sh
+++ setup.sh
@@ -72,20 +72,42 @@ enabled=1
 EOF
 
     echo "Step 2. Install prerequisites"
-    for pkg in arrow-devel-$ARROW_VER.el6       \
-               devtoolset-3-runtime             \
-               devtoolset-3-toolchain           \
-               gcc                              \
-               git                              \
-               libpqxx-devel                    \
-               log4cxx-devel                    \
-               pcre-devel                       \
-               protobuf-devel-2.4.1             \
-               rpm-build                        \
-               rpmdevtools                      \
-               scidb-$SCIDB_VER                 \
-               scidb-$SCIDB_VER-dev             \
-               scidb-$SCIDB_VER-libboost-devel  \
+
+    url=http://mirror.centos.org/centos/6/sclo/x86_64/rh/devtoolset-3
+    pkg_lst=""
+    for pkg in binutils-2.24-18                 \
+               dwz-0.11-1.1                     \
+               elfutils-0.161-1                 \
+               elfutils-libelf-0.161-1          \
+               elfutils-libs-0.161-1            \
+               gcc-4.9.2-6.2                    \
+               gcc-c++-4.9.2-6.2                \
+               gcc-gfortran-4.9.2-6.2           \
+               gdb-7.8.2-38                     \
+               libquadmath-devel-4.9.2-6.2      \
+               libstdc++-devel-4.9.2-6.2        \
+               ltrace-0.7.91-10                 \
+               memstomp-0.1.5-3                 \
+               runtime-3.1-12                   \
+               strace-4.8-8                     \
+               toolchain-3.1-12
+    do
+        pkg_lst="$pkg_lst $url/devtoolset-3-$pkg.el6.x86_64.rpm"
+    done
+    yum install --assumeyes $pkg_lst
+
+    for pkg in arrow-devel-$ARROW_VER.el6                               \
+               gcc                                                      \
+               git                                                      \
+               libpqxx-devel                                            \
+               log4cxx-devel                                            \
+               pcre-devel                                               \
+               protobuf-devel-2.4.1                                     \
+               rpm-build                                                \
+               rpmdevtools                                              \
+               scidb-$SCIDB_VER                                         \
+               scidb-$SCIDB_VER-dev                                     \
+               scidb-$SCIDB_VER-libboost-devel                          \
                zlib-devel
     do
         yum install --assumeyes $pkg
