eefa3011428710129ab19cf7be515f42c5b3a3ff
Rares Vernica <rvernica@gmail.com>
2020-04-28 22:21:36 -0700

    Use latest shim release

--- extra-scidb-libs.sh
+++ extra-scidb-libs.sh
@@ -95,7 +95,7 @@ declare -a libs=(
     "accelerated_io_tools" "v19.11"
     "equi_join"            "v19.11"
     "grouped_aggregate"    "v19.11"
-    "shim"                 "master"
+    "shim"                 "v19.11.2"
     "stream"               "v19.11"
     "superfunpack"         "master"
 )
