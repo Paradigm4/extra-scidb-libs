c40a04f11901817c6c1f280111653d8fb43b705c
Rares Vernica <rvernica@gmail.com>
2020-04-30 13:31:38 -0700

    Version 1 for SciDB 19.11

--- extra-scidb-libs.sh
+++ extra-scidb-libs.sh
@@ -92,12 +92,12 @@ mkdir -p $work_dir/extra-scidb-libs-${SCIDB_VER:=19.11}-$PKG_VER
 
 # The following array should contain tuples of the repo name and the branch to get.
 declare -a libs=(
-    "accelerated_io_tools" "v19.11"
-    "equi_join"            "v19.11"
-    "grouped_aggregate"    "v19.11"
-    "shim"                 "v19.11.2"
-    "stream"               "v19.11"
-    "superfunpack"         "master"
+    "accelerated_io_tools" "v19.11.1"
+    "equi_join"            "v19.11.1"
+    "grouped_aggregate"    "v19.11.1"
+    "shim"                 "v19.11.1"
+    "stream"               "v19.11.1"
+    "superfunpack"         "v19.11.1"
 )
 
 downloadLibs "${libs[@]}"
